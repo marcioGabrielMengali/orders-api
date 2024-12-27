3381e48bfed5eeebb7f350e43d62f34757d4b5cb
Marcio gabriel Mengali <marciogabrielmengali@MacBook-Air-de-Marcio.local>
2024-12-26 22:11:23 -0300

    fix: env file

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,28 +1,19 @@
 import 'dotenv/config'
-import { z, ZodError, ZodIssue } from 'zod'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
-  APP_PORT: z.string().transform((val) => {
-    const num = Number(val)
-    if(isNaN(num)){
-        const issue: ZodIssue = {
-            path: ['APP_PORT'],
-            message: 'Invalid environmets schema Error APP_PORT is Not a Number',
-            code: 'custom'
-        }
-        throw new ZodError([issue])
-    }
-    return num
-  }),
+  APP_PORT: z.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false){
-    console.log('Invalid environments schema:', {erros: JSON.stringify(_env.error.format())});
-    throw new Error('Invalid environments schema')
+if (_env.success === false) {
+  console.log('Invalid environments schema:', {
+    erros: JSON.stringify(_env.error.format()),
+  })
+  throw new Error('Invalid environments schema')
 }
 
 export const env = _env.data
